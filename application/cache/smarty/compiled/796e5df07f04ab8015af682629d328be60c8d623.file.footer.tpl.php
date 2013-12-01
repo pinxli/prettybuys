58eb8f9661019e62f3cd334149fc8610b76e6728
pinxli <pliwanagan@hotmail.com>
2013-12-01 10:46:43 +0800

    empty directory

---
+++ application/cache/smarty/compiled/796e5df07f04ab8015af682629d328be60c8d623.file.footer.tpl.php
@@ -0,0 +1,191 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2013-12-01 02:42:28
+         compiled from "application/views/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1846820294529aa214a65f73-74914424%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '796e5df07f04ab8015af682629d328be60c8d623' => 
+    array (
+      0 => 'application/views/footer.tpl',
+      1 => 1385864010,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1846820294529aa214a65f73-74914424',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_529aa214a6ca1',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_529aa214a6ca1')) {function content_529aa214a6ca1($_smarty_tpl) {?><!-- start: Footer -->
+	<footer>
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row-fluid">
+				
+				<!-- start: Footer Navigation -->
+				<div class="span12 nav">
+					
+					<ul class="scroll-nav">
+						<li class="hidden-phone"><a href="index.html">Home</a></li>
+									  			<li class="hidden-phone"><a class="scroll" href="#about">About</a></li>
+						<li class="hidden-phone"><a class="scroll" href="#team">Team</a></li>
+						<li class="hidden-phone"><a class="scroll" href="#pricing">Pricing</a></li>
+						<li class="hidden-phone"><a class="scroll" href="#projects">Projects</a></li>
+						<li class="hidden-phone"><a class="scroll" href="#blog">Blog</a></li>
+						<li class="hidden-phone"><a class="scroll" href="#contact">Contact</a></li>
+												<li><a class="back-to-top" href="#">Back To Top</a></li>
+					</ul>
+					
+				</div>
+				<!-- end: Footer Navigation -->
+			
+			</div>
+			<!-- end: Row -->		
+			
+			<!-- start: Row -->
+			<div class="row-fluid">	
+
+				<!-- start: Latest Tweets -->
+				<div class="span7">
+					
+					<h4>Twitter news</h4>
+					<div id="twitter-nav">
+						<a href="#" id="tweet-prev"></a>
+						<a href="#" id="tweet-next"></a>	
+					</div>	
+					<div id="twitter">
+						<ul class="slides">
+						</ul>
+					</div>
+				</div>
+				<!-- end: Latest Tweets -->
+
+				
+				<!-- start: Newsletter -->
+				<div class="span5">
+					
+					<h4>Newsletter</h4>
+					
+					<!-- start: Row -->
+					<div class="row-fluid">
+					
+						<form id="newsletter">
+						
+							<input type="text" class="name span5" placeholder="your name">
+							<input type="text" class="email span5" placeholder="your email">
+							<input type="button" class="email span2" value="ok">
+						
+						</form>
+					
+					</div>
+					<!-- end: Row -->
+	
+				</div>
+				<!-- end: Newsletter -->
+				
+			</div>
+			<!-- end: Row -->
+		
+		</div>
+		<!-- end: Container  -->	
+
+	</footer>
+	<!-- end: Footer -->
+	
+	<!-- start: Copyright -->
+	<div id="copyright">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row-fluid">
+
+				<!-- start: Owner -->
+				<div id="owner" class="span6">
+					
+					&copy; 2013, <a href="http://bootstrapmaster.com" alt="Bootstrap Themes">creativeLabs</a>. Designed by <a href="http://bootstrapmaster.com" alt="Bootstrap Templates">Bootstrap Master</a> in Poland <img src="assets/img/poland.png" class="img-retina" alt="Poland" style="margin-top:-4px">
+					
+				</div>
+				<!-- end: Owner -->
+
+				<!-- start: Follow Us -->
+				<div id="follow" class="span6">
+					
+					Follow Us:
+					
+					<a href="#" class="social-dribbble"></a>
+					<a href="http://twitter.com/BootstrapMaster" class="social-twitter"></a>
+					<a href="#" class="social-flickr"></a>
+					<a href="http://facebook.com/BootstrapMaster" class="social-facebook"></a>
+					<a href="#" class="social-youtube"></a>
+					
+				</div>
+				<!-- end: Follow Us -->
+			
+			</div>
+			<!-- end: Row -->
+			
+		</div>	
+		<!-- end: Container -->
+		
+	</div>
+	<!-- end: Copyright -->
+	
+	<!-- start: Fixed Menu -->
+	<div id="fixed-menu">
+		
+		<!-- start: Container -->
+		<div class="container menu">
+		
+			<ul class="scroll-nav">
+				<li class="hidden-phone"><a href="index.html">Home</a></li>
+					  			<li class="hidden-phone"><a class="scroll" href="#about">About</a></li>
+				<li class="hidden-phone"><a class="scroll" href="#team">Team</a></li>
+				<li class="hidden-phone"><a class="scroll" href="#pricing">Pricing</a></li>
+				<li class="hidden-phone"><a class="scroll" href="#projects">Projects</a></li>
+				<li class="hidden-phone"><a class="scroll" href="#blog">Blog</a></li>
+				<li class="hidden-phone"><a class="scroll" href="#contact">Contact</a></li>
+								<li><a class="back-to-top" href="">Back To Top</a></li>
+			</ul>
+		
+		</div>	
+		<!-- end: Container -->	
+		
+	</div>
+	<!-- end: Fixed Menu -->		
+
+<!-- start: Java Script -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery-1.10.2.min.js"></script>
+<script src="assets/js/jquery.isotope.min.js"></script>
+<script src="assets/js/jquery.imagesloaded.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/flexslider.js"></script>
+<script src="assets/js/carousel.js"></script>
+<script src="assets/js/jquery.fancybox.js"></script>
+<script src="assets/js/twitter.js"></script>
+<script src="assets/js/jquery.placeholder.min.js"></script>
+
+<script src="assets/js/jquery-easing-1.3.js"></script>
+<script src="assets/js/jquery-transit-modified.js"></script>
+<script src="assets/js/layerslider.transitions.js"></script>
+<script src="assets/js/layerslider.kreaturamedia.jquery.js"></script>
+
+<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script src="assets/js/jquery.gmap.min.js"></script>
+
+<script src="assets/js/less-1.3.3.min.js"></script>
+
+<script defer="defer" src="assets/js/modernizr.js"></script>
+<script defer="defer" src="assets/js/custom.js"></script>
+<!-- end: Java Script -->
+
+</body>
+</html><?php }} ?>
