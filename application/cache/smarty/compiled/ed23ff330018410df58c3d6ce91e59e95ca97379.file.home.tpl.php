82aff6500fcaccb1493ac69172d5cc5692cd1910
pinxli <pliwanagan@hotmail.com>
2013-12-01 10:27:45 +0800

    cache folder

---
+++ application/cache/smarty/compiled/ed23ff330018410df58c3d6ce91e59e95ca97379.file.home.tpl.php
@@ -0,0 +1,173 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2013-12-01 02:15:13
+         compiled from "application/modules/home/views/home.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:6731488625290ad137843c1-82536668%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'ed23ff330018410df58c3d6ce91e59e95ca97379' => 
+    array (
+      0 => 'application/modules/home/views/home.tpl',
+      1 => 1385864010,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '6731488625290ad137843c1-82536668',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5290ad1378b5a',
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5290ad1378b5a')) {function content_5290ad1378b5a($_smarty_tpl) {?><!-- start: Section Home -->
+	<section id="home">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Featured Slider -->
+			<div id="layerslider-container">
+
+				<div id="layerslider-main" style="width: 1070px; height: 450px;">
+					
+					<!-- start: 1st Slide -->
+					<div class="ls-layer" style="slidedirection: top; slidedelay: 7000;">
+
+						<h1 class="ls-s1" style="top:130px; left: 0px; slidedirection : left;">
+							Powerfull One Page Theme
+						</h1>
+						
+						<h2 class="ls-s1" style="top:180px; left: 0px; slidedirection : top;">
+							<span>ONE Bootstrap Theme</span>
+						</h2>
+						
+						<img class="ls-s3 img-retina" src="assets/img/macbook.png" alt="slider" style=" position: absolute; top:30px; left: 400px; slidedirection : top; delayin : 500;">
+
+						<h3  class="ls-s2" style="top:230px; left: 0px; slidedirection : bottom; slideoutdirection : left; durationin : 2000; durationout : 1000; easingin : easeOutElastic; easingout : easeInOutQuint; delayin : 1000; showuntil : 2000;">
+							probably the best bootstrap theme
+						</h3>
+						
+						<h4  class="ls-s3" style="top:260px; left: 0px; slidedirection : top; slideoutdirection : bottom; durationin : 1500; durationout : 1000; easingin : easeOutBack; easingout : easeInOutQuint; delayin : 1000; showuntil : 1900;"> 
+							on the wrapbootstrap!
+						</h4>
+						
+						<h3  class="ls-s3" style="top:230px; left: 0px; slidedirection : left; slideoutdirection : right; durationin : 1200; durationout : 1200; easingin : easeInOutQuart; easingout : easeInOutQuart; delayin : 5000;">
+							smart, clean & modern bootstrap theme!
+						</h3>
+						
+						<h4 class="ls-s3" style="top:260px; left: 0px; slidedirection : right; slideoutdirection : left; durationin : 1200; durationout : 1200; easingin : easeInOutQuart; easingout : easeInOutQuint; delayin : 5000;">
+							with a lot of features!
+						</h4>
+						
+						<a class="ls-s4 btn btn-large btn-success" href="https://wrapbootstrap.com/user/creativelabs" style="top:300px; left: 40px; slidedirection : bottom; slideoutdirection : left; durationin : 1400; durationout : 1400; easingin : easeInOutQuart; easingout : easeInOutQuint; delayin : 5000;">Buy it now</a>
+
+					</div>
+					<!-- end: 1st Slide -->
+					
+					<!-- start: 2nd Slide -->
+					<div class="ls-layer layer2" style="slidedirection: right; slidedelay: 4000;">
+						
+						<img class="ls-s1 img-retina" src="assets/img/responsive-ipad-black.png" alt="slider" style="position: absolute; z-index: -1; top: 10px; left: 50px; slidedirection : right; slideoutdirection : left;  delayin : 500; showuntil : 3000; ">
+
+						<img class="ls-s2 img-retina" src="assets/img/responsive-iphone-black.png" alt="slider" style="position: absolute; top: 10px; left: 50px; slidedirection : right; slideoutdirection : left;  delayin : 1000; showuntil : 3100; ">
+						
+						<img class="ls-s3 img-retina" src="assets/img/responsive-iphone-white.png" alt="slider" style="position: absolute; top: 10px; left: 50px; slidedirection : right; slideoutdirection : left;  delayin : 1500; showuntil : 3200; ">
+
+						<h1 class="ls-s4" style="top:130px; left: 490px; slidedirection : left; showuntil : 3200;">
+							Responsive Design
+						</h1>
+						<h2 class="ls-s4" style="top:180px; left: 490px; slidedirection : top; showuntil : 3200;">
+							<span>Working on every devices</span>
+						</h2>
+						<h3  class="ls-s5" style="top:230px; left: 490px; slidedirection : bottom; slideoutdirection : left; durationin : 2000; durationout : 1000; easingin : easeOutElastic; easingout : easeInOutQuint; delayin : 1000; showuntil : 3000;">
+							specialy optimized to retina displays
+						</h3>
+						<h4  class="ls-s5" style="top:260px; left: 490px; slidedirection : top; slideoutdirection : bottom; durationin : 1500; durationout : 1000; easingin : easeOutBack; easingout : easeInOutQuint; delayin : 1000; showuntil : 2900;"> 
+							tested on iPhone 4&amp;5, iPad 4, Macbook PRO
+						</h4>
+						
+
+					</div>
+					<!-- end: 2nd Slide -->
+
+					<!-- start: 3rd Slide -->
+					<div class="ls-layer" style="slidedirection: right; slidedelay: 4000;">
+
+						<img class="ls-s1 img-retina" src="assets/img/retina-black.png" alt="slider" style="position: absolute; top: 20px; left: 450px; slidedirection : right; slideoutdirection : right;">
+						
+						<img class="ls-s2 img-retina" src="assets/img/retina-white.png" alt="slider" style="position: absolute; top: 25px; left: 450px; slidedirection : right; slideoutdirection : right; delayin : 200;">
+
+						<h1 class="ls-s3" style="top:100px; left: 40px; slidedirection : left;">
+							Retina and High Resolutions
+						</h1>
+						
+						<h1 class="ls-s3" style="top:140px; left: 40px; slidedirection : left;">
+							Screens Support
+						</h1>
+						
+						<a class="ls-s4 btn btn-large btn-info" href="https://wrapbootstrap.com/user/creativelabs" style="top:300px; left: 40px; slidedirection : bottom; slideoutdirection : left; durationin : 1400; durationout : 1400; easingin : easeInOutQuart; easingout : easeInOutQuint; delayin : 1000;">Try it now!</a>
+						
+						<h3  class="ls-s5" style="top:220px; left: 40px; slidedirection : bottom; slideoutdirection : left; durationin : 2000; durationout : 1000; easingin : easeOutElastic; easingout : easeInOutQuint; showuntil : 3000;">
+							<span>Font Face & SVG Icons</span>
+						</h3>
+						
+						<h4  class="ls-s6" style="top:250px; left: 40px; slidedirection : top; slideoutdirection : bottom; durationin : 1500; durationout : 1000; easingin : easeOutBack; easingout : easeInOutQuint; showuntil : 3000;"> 
+							<span><b>Perfect Pixels</b></span>
+						</h4>
+
+					</div>
+					<!-- end: 3rd Slide -->
+					
+				</div>
+
+			</div>
+			<!-- end: Featured Slider -->
+						
+			<!-- start: Featured Icons -->
+			<div class="row-fluid featured-icons">        
+			
+				<div class="span3">
+					<i class="li_phone"></i>
+					<h3>Responsive & Perfect Pixels</h3>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+				</div>
+				
+				<div class="span3">
+					<i class="li_bulb"></i>
+					<h3>Creative & Clean Design</h3>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+				</div>
+				
+				<div class="span3">
+					<i class="li_paperplane"></i>
+					<h3>Awesome Features</h3>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+				</div>
+				
+				<div class="span3">
+					<i class="li_params"></i>
+					<h3>Easy To Customize</h3>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+				</div>
+			
+			</div>
+			<!-- end: Featured Icons -->
+			
+			<hr>
+			
+			<!-- start: Hero Unit - Main hero unit for a primary marketing message or call to action -->
+			 <div class="hero-unit">
+				<h2>
+					ONE Bootstrap Theme is incredibly powerfull and responsive template with modern and clear design.
+				</h2>
+				<h3> 
+					ONE has many great features like: awesome LayerSlider 4 ($50), FlexSlider 2, 400+ Glyphicons PRO ($59), 200+ Font Awesome Icons, Social Icons, Charts, Retina Display Support and many. <strong>If you buy now Clear Bootstrap Theme you save $109.</strong>
+				</h3>
+			</div>
+			<!-- end: Hero Unit -->
+
+		</div>
+		<!-- end: Container -->
+	
+	</section>
+	<!-- end: Section Home --><?php }} ?>
